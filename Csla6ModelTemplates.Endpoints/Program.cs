b3fd65217cc8dcedcac83d7ad69375d490a21014
Mármarosi József <marmarosi@logikum.hu>
2023-03-18 13:59:12 +0100

    feat: add reference to ardalis endpoints

--- Csla6ModelTemplates.Endpoints/Program.cs
+++ Csla6ModelTemplates.Endpoints/Program.cs
@@ -1,19 +1,86 @@
+using Csla.Configuration;
+using Csla6ModelTemplates.Configuration;
+using Csla6ModelTemplates.Dal;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddCors(options => {
+    options.AddPolicy(
+        "Csla6ModelTemplatesPolicy",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+        );
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "CSLA 6 REST API",
+            Description = string.Format("CSLA 6 model templates used in REST API ● Version {0}",
+                Assembly
+                    .GetEntryAssembly()
+                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                    .InformationalVersion
+            )
+        }
+    );
+    string xmlFile = $"{builder.Environment.ApplicationName}.xml";
+    string xmlPath = Path.Combine(builder.Environment.ContentRootPath, xmlFile);
+    o.IncludeXmlComments(xmlPath, true);
+});
+
+// Configure data access layer.
+builder.Services.AddSqlServerDal();
+builder.Services.AddSingleton(typeof(ITransactionOptions), new TransactionOptions(false));
+
+// If using Kestrel:
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+// If using IIS:
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+
+// Configure CSLA.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddCsla(o => o
+    .AddAspNetCore()
+    .DataPortal(dpo => dpo
+        .UseLocalProxy(options => {
+            options.UseLocalScope = true;
+            options.FlowSynchronizationContext = false;
+        })
+    )
+);
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.RunSeeders(app.Environment.IsDevelopment(), app.Environment.ContentRootPath);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(o => o.DocExpansion(DocExpansion.None));
 }
 
 app.UseHttpsRedirection();
