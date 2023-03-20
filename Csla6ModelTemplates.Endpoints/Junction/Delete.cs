1d0190bf46ececefb3c53511895b47b3a83f037b
Mármarosi József <marmarosi@logikum.hu>
2023-03-20 11:03:50 +0100

    feat: add junction endpoints

---
+++ Csla6ModelTemplates.Endpoints/Junction/Delete.cs
@@ -0,0 +1,74 @@
+﻿using Ardalis.ApiEndpoints;
+using Csla;
+using Csla6ModelTemplates.Contracts;
+using Csla6ModelTemplates.Contracts.Junction.Edit;
+using Csla6ModelTemplates.Models.Junction.Edit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+
+namespace Csla6ModelTemplates.Endpoints.Junction
+{
+    /// <summary>
+    /// Deletes the specified group.
+    /// </summary>
+    [Route(Routes.Junction)]
+    public class Delete : EndpointBaseAsync
+        .WithRequest<string>
+        .WithoutResult
+    {
+        internal ILogger Logger { get; private set; }
+        internal IDataPortalFactory Factory { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance of the endpoint.
+        /// </summary>
+        /// <param name="logger">The application logging service.</param>
+        /// <param name="factory">The data portal factory.</param>
+        public Delete(
+            ILogger<Delete> logger,
+            IDataPortalFactory factory
+            )
+        {
+            Logger = logger;
+            Factory = factory;
+        }
+
+        /// <summary>
+        /// Deletes the specified group.
+        /// </summary>
+        /// <param name="id">The identifier of the group.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        [HttpDelete("{id}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [SwaggerResponse(StatusCodes.Status204NoContent)]
+        [SwaggerOperation(
+            Summary = "Deletes the specified group.",
+            Description = "Deletes the specified group.<br>" +
+                "Criteria:<br>{<br>" +
+                "&nbsp;&nbsp;&nbsp;&nbsp;groupId: string<br>" +
+                "}",
+            OperationId = "Group.Delete",
+            Tags = new[] { "Junction" })
+        ]
+        public override async Task<ActionResult> HandleAsync(
+            string id,
+            CancellationToken cancellationToken = default
+            )
+        {
+            try
+            {
+                return await Run.RetryOnDeadlock(async () =>
+                {
+                    await Task.Run(() => Group.Delete(Factory, id));
+                    return NoContent();
+                });
+            }
+            catch (Exception ex)
+            {
+                return Helper.HandleError(this, Logger, ex);
+            }
+        }
+    }
+}
