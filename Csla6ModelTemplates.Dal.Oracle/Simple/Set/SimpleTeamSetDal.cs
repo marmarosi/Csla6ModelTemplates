ecb6f127d5258924c4b684829ba46bf919385d8a
Mármarosi József <marmarosi@logikum.hu>
2023-03-30 22:58:02 +0200

    feat: add oracle data access layer

---
+++ Csla6ModelTemplates.Dal.Oracle/Simple/Set/SimpleTeamSetDal.cs
@@ -0,0 +1,59 @@
+using Csla6ModelTemplates.Contracts.Simple.Set;
+using Microsoft.EntityFrameworkCore;
+
+namespace Csla6ModelTemplates.Dal.Oracle.Simple.Set
+{
+    /// <summary>
+    /// Implements the data access functions of the editable team collection.
+    /// </summary>
+    [DalImplementation]
+    public class SimpleTeamSetDal : DalBase<OracleContext>, ISimpleTeamSetDal
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Instantiates the data access object.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        public SimpleTeamSetDal(
+            OracleContext dbContext
+            )
+        {
+            DbContext = dbContext;
+        }
+
+        #endregion Constructor
+
+        #region Fetch
+
+        /// <summary>
+        /// Gets the specified team set.
+        /// </summary>
+        /// <param name="criteria">The criteria of the team set.</param>
+        /// <returns>The requested team set.</returns>
+        public List<SimpleTeamSetItemDao> Fetch(
+            SimpleTeamSetCriteria criteria
+            )
+        {
+            // Get the specified team set.
+            var list = DbContext.Teams
+                .Where(e =>
+                    criteria.TeamName == null || e.TeamName.Contains(criteria.TeamName)
+                )
+                .Select(e => new SimpleTeamSetItemDao
+                {
+                    TeamKey = e.TeamKey,
+                    TeamCode = e.TeamCode,
+                    TeamName = e.TeamName,
+                    Timestamp = e.Timestamp
+                })
+                .OrderBy(o => o.TeamName)
+                .AsNoTracking()
+                .ToList();
+
+            return list;
+        }
+
+        #endregion Fetch
+    }
+}
